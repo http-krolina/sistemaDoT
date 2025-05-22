f8151ef5a4ea62ae0738e9ebcaa7879dacdbd810
http-krolina <carola061200@gmail.com>
2025-05-22 19:15:51 -0300

    alter

--- D.T/src/App.jsx
+++ D.T/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Cadastro from "./components/Cadastro/Cadastro";
-import Login from "./components/Login/Login";
+import Cadastro from "./pages/Cadastro/Cadastro";
+import Login from "./Pages/Login/Login";
 import styles from "./App.module.css";
 
 function App() {
